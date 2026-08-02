c131cb4c3abe44912f56fcb90740c722d6514a27
marius.danciu <marius.danciu@gmail.com>
2026-08-02 12:59:45 +0300

    fix: use debug: logs
    Signed-off-by: marius.danciu <marius.danciu@gmail.com>

--- services/flight/src/flight/service.rs
+++ services/flight/src/flight/service.rs
@@ -16,7 +16,7 @@ use prost::Message;
 use prost::bytes::Bytes;
 use std::sync::Arc;
 use tonic::{Request, Response, Status};
-use tracing::info;
+use tracing::{debug, info};
 
 pub struct TabularDataService {
     connectors_registry: Arc<ConnectorsRegistry>,
@@ -146,7 +146,7 @@ impl FlightSqlService for TabularDataService {
         query: CommandStatementQuery,
         request: Request<FlightDescriptor>,
     ) -> Result<Response<FlightInfo>, Status> {
-        info!("Received SQL Query: '{}'", query.query);
+        debug!("Received SQL query: '{}'", query.query);
 
         let metadata = request.metadata();
         let connection_id = metadata
@@ -210,7 +210,7 @@ impl FlightSqlService for TabularDataService {
     ) -> Result<Response<<Self as FlightService>::DoGetStream>, Status> {
         let query = String::from_utf8(ticket.statement_handle.to_vec())
             .map_err(|_| Status::invalid_argument("Invalid statement handle"))?;
-        info!("do_get_statement: '{}'", query);
+        debug!("Retrieving data with SQL query: '{}'", query);
 
         let metadata = request.metadata();
         let connection_id = metadata
