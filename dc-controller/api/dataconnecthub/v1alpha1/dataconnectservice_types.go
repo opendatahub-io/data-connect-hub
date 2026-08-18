62488486380b43bf38bb87e8873f33dfb293cf46
marius.danciu <marius.danciu@gmail.com>
2026-08-18 11:02:01 +0300

    fix: replace status.hostname with status.addresses as per latest ADR.
    Signed-off-by: marius.danciu <marius.danciu@gmail.com>

--- dc-controller/api/dataconnecthub/v1alpha1/dataconnectservice_types.go
+++ dc-controller/api/dataconnecthub/v1alpha1/dataconnectservice_types.go
@@ -110,6 +110,16 @@ type DataConnectServiceSpec struct {
 	Gateway *Gateway `json:"gateway,omitempty"`
 }
 
+type Addresses struct {
+	// type can be "hostname"
+	// +optional
+	Type string `json:"type,omitempty"`
+
+	// the value of the address
+	// +optional
+	Value string `json:"httpRoute,omitempty"`
+}
+
 // DataConnectServiceStatus defines the observed state of DataConnectService.
 type DataConnectServiceStatus struct {
 	// observedGeneration is the last generation observed by the controller
@@ -128,9 +138,9 @@ type DataConnectServiceStatus struct {
 	// +optional
 	Phase string `json:"phase,omitempty"`
 
-	// hostname is the hostname where the service is reachable
+	// addresses is the addresses where the service is reachable
 	// +optional
-	Hostname string `json:"hostname,omitempty"`
+	Adresses []Addresses `json:"addresses,omitempty"`
 
 	// httpRoute is the name of the HTTPRoute resource created for this service
 	// +optional
