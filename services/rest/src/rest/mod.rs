32aae0c3c9f9c71cf89951680a17529195150826
marius.danciu <marius.danciu@gmail.com>
2026-08-29 15:34:56 +0300

    lint: fix lint
    Signed-off-by: marius.danciu <marius.danciu@gmail.com>

--- services/rest/src/rest/mod.rs
+++ services/rest/src/rest/mod.rs
@@ -1,13 +1,3 @@
 pub mod endpoints;
 pub mod errors;
 pub mod middleware;
-
-use std::sync::Arc;
-use std::sync::atomic::{AtomicBool, Ordering};
-
-use crate::clients::flight::FlightClient;
-use commons::api::connection_types::DataConnectionTypeResource;
-use commons::api::connection_types::DataConnectionTypeStatus;
-use commons::api::storage::MetaStore;
-use errors::RestErrorResponse;
-
