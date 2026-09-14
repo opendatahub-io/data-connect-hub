132a75d39492c17b084ea262212377fe5ea36ebb
Angad Singh <12499805+angaduom@users.noreply.github.com>
2026-09-14 17:43:56 -0400

    fix: resolve Flight instance CI failures
    Signed-off-by: Angad Singh <12499805+angaduom@users.noreply.github.com>

--- dc-controller/internal/controller/dataconnectservice_controller.go
+++ dc-controller/internal/controller/dataconnectservice_controller.go
@@ -70,8 +70,11 @@ const (
 	nameDataConnectHub = "data-connect-hub"
 	nameDatabaseConfig = "dch-database-config"
 
-	kindDeployment = "Deployment"
-	kindConfigMap  = "ConfigMap"
+	kindDeployment         = "Deployment"
+	kindConfigMap          = "ConfigMap"
+	kindService            = "Service"
+	kindServiceAccount     = "ServiceAccount"
+	kindClusterRoleBinding = "ClusterRoleBinding"
 
 	repoURL = "https://github.com/opendatahub-io/data-connect-hub"
 
