4ab14c815ed71446f8bb07d29a517542207bec88
Angad Singh <angsingh@angsingh-mac.redhat.corp>
2026-09-24 13:06:55 -0400

    fix(e2e): replace unavailable MinIO image

--- hack/install-milvus.sh
+++ hack/install-milvus.sh
@@ -228,8 +228,8 @@ else
         --set pulsarv3.enabled=false
         --set etcd.replicaCount=1
         --set minio.mode=standalone
-        --set minio.image.repository=quay.io/minio/minio
-        --set minio.image.tag=RELEASE.2025-04-03T14-56-28Z
+        --set minio.image.repository=quay.io/thanos/minio
+        --set minio.image.tag=RELEASE.2025-09-07T16-13-09Z
         --set minio.resources.requests.memory=512Mi
         --set standalone.resources.requests.memory=512Mi
         --set standalone.resources.requests.cpu=200m
